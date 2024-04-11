47944a95d8f6b9968a27feff75f951f5bd56b570
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2024-04-11 13:27:09 -0600

    update test and add cors module

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const router = express.Router();
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(morgan("tiny")); //Log request
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5555"); // Update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 const recommendationController = require("./controllers/Recommendations");
 router.get("/recommendations", recommendationController.getRecommendation); 
 
